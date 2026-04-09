85f949dc3b2c40c027e32439e2592ace9aa9a7b6
Marwan-Mamdouh <marwanabdalmagied@gmail.com>
2026-04-09 20:59:45 +0200

    add react compiler for better reloads and faster builds

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+	// Next.js 16 specific optimizations
+	reactStrictMode: true,
+	reactCompiler: true,
 };
 
 export default nextConfig;
